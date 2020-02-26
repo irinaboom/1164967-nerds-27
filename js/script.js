26b411f270cec43e2b4a2b9212b48bc031a5083c
irinaboom <57409510+irinaboom@users.noreply.github.com>
2020-02-26 17:24:20 +0300

    на защиту

--- js/script.js
+++ js/script.js
@@ -31,24 +31,28 @@
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+		popup.classList.remove("modal-error");
   });
-  
+
   form.addEventListener("submit", function (evt) {
     if (!login.value || !email.value) {
       evt.preventDefault();
-      console.log("Нужно ввести имя и e-mail");
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
     } else {
       if (isStorageSupport) {
         localStorage.setItem("login", login.value);
       }
     }
   });
-  
+
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
       }
     }
   });
